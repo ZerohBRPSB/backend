4682edf12f903aa9934187be8ac32f4492a20b7b
ZerohBRPSB <miguelgomes13@live.com>
2019-04-10 22:34:28 +0000

    Adicionado as variaveis de ambiente

--- src/models/File.js
+++ src/models/File.js
@@ -20,6 +20,7 @@ const File = new mongoose.Schema(
 );
 
 File.virtual("url").get(function() {
+    const url = process.env.URL || "http://localhost:3000";
     return `http://localhost:3000/files/${encodeURIComponent(this.path)}`;
 });
 
